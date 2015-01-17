29f94748bc82c386a8926aaabea1cd075c08fa4c
Casing Chu <casingchu@nationnotice-lm.corp.yahoo.com>
2015-01-16 23:39:56 -0800

    Initial Commit

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Casing Chu on 1/16/15.
+//  Copyright (c) 2015 casing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
