bd1bcd3ab28f9e4c0b7ac8705b16ef47463359fc
Casing Chu <casing@gmail.com>
2015-01-17 13:40:27 -0800

    TipCalculator Initial Check in

---
+++ TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Casing Chu on 1/16/15.
+//  Copyright (c) 2015 casing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
